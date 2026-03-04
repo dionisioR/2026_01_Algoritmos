88ca85ce981b3607ee1c40a19cf78b3857ab056b
dionisioR <rodrigo.b.dionisio@gmail.com>
2026-03-04 14:52:07 -0300

    Update 226-03-04 14h51
    Update 226-03-04 14h51

---
+++ codigo/app.js
@@ -0,0 +1,7 @@
+// let nome = "FATEC - Faculdade de Tecnologia de Guaratinguetá"
+
+
+let nome = prompt("Digite um texto:") // entrda de dados
+
+// saída de dados
+document.getElementById("resultado").innerHTML = "Você digitou o nome: " + nome

0f5e201dc51610ec38c2ebbf9a9df97ea0edf306
dionisioR <rodrigo.b.dionisio@gmail.com>
2026-03-04 15:52:52 -0300

    Update 2026-03-04   15h51
    Update 2026-03-04   15h51

---
+++ exercicio_01/app.js
@@ -0,0 +1,15 @@
+// let nome = prompt("Digite seu nome:")
+// document.getElementById('resultado').innerHTML = nome
+// console.log(nome);
+
+// document.getElementById('resultado').innerHTML = "Olá, mundo!"
+// console.log('Olá mundo!');
+
+// let mundo = "Olá, mundo!"
+// document.getElementById('resultado').innerHTML = mundo
+// console.log(mundo);
+
+let mundo = prompt("Digite uma mensagem:")
+document.getElementById('resultado').innerHTML = mundo
+console.log(mundo);
+alert(mundo)

bb8397f2f797e507e2b35dc21f87c359efa49d22
dionisioR <rodrigo.b.dionisio@gmail.com>
2026-03-18 17:16:53 -0300

    Update 2026-03-18   17h16
    Update 2026-03-18   17h16

---
+++ exercicio_17/app.js
@@ -0,0 +1,16 @@
+let num = 10 // prompt("Digite seu nome:")
+
+// let antecessor = num - 1
+// let sucessor = num + 1
+
+document.getElementById('resultado').innerHTML = `
+O número digitado foi ${num}, seu antecessor é ${num - 1} e seu sucessor é ${num + 1}.
+`
+let num_2 = 55
+console.log(++num_2)  //56
+console.log(num_2);
+console.log('-------------');
+num_2++  // 57
+++num_2  // 58
+++num_2 // 59
+console.log(num_2);

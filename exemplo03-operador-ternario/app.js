87aaac8cd1ec2bafa5fb1fb17aaa59515a1ba67e
dionisioR <rodrigo.b.dionisio@gmail.com>
2026-04-07 14:13:05 -0300

    Update
    Update dia 01-04-2026

---
+++ exemplo03-operador-ternario/app.js
@@ -0,0 +1,12 @@
+let n_1 = prompt("Digite um número:")
+
+
+let resposta = (n_1 % 2 == 0 )? "PAR" : "ÍMPAR"
+
+document.getElementById("resultado").innerHTML = resposta
+
+
+
+console.log((n_1 % 2 == 0 )? "PAR" : "ÍMPAR");
+
+// document.getElementById("resultado").innerHTML = (n_1 % 2 == 0 )? "PAR" : "ÍMPAR"
---
+++ exemplo04-if-else/app.js
@@ -0,0 +1,61 @@
+let dia = prompt("Dia da semana (0-6):");
+
+if(dia == 0){
+    console.log("Domingo");
+}else if(dia == 1){
+    console.log("Segunda");
+}else if(dia == 2){
+    console.log("Terça");
+}else if(dia == 3){
+    console.log("Quarta");
+}else if(dia == 4){
+    console.log("Quinta");
+}else if(dia == 5){
+    console.log("Sexta");
+}else if(dia == 6){
+    console.log("Sábado");
+}else{
+    console.log('Não é um dia da semana válido!!!');
+}
+
+console.log("fim de código");
+
+
+
+
+
+//---------------------
+// exemplo 01
+// if(dia == 0){
+//     console.log("Domingo");
+// }
+
+//---------------------
+// exemplo 02
+
+// if(dia == 0){
+    console.log("Domingo");
+// }else{
+//     console.log("Não é domingo");
+// }
+
+//---------------------
+// exemplo 03
+// if(dia == 0){
+//     console.log("Domingo");
+// }else{
+//     if(dia == 1){
+//         console.log("Segunda");
+//     }else{
+//         if(dia == 2){
+//             console.log("Terça");
+//         }else{
+//             if(dia == 3){
+//                 console.log("Quarta");
+//             }else{
+//                 console.log('Dia inválido!!!');
+//             }
+//         }
+    
+//     }
+// }

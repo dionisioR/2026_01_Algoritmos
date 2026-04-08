c9885571d4181e79b58c2b2aaebced403bcf530c
dionisioR <rodrigo.b.dionisio@gmail.com>
2026-04-08 16:39:48 -0300

    Update 2026-04-08 16h39
    Update 2026-04-08 16h39  revisão

---
+++ exemplo05-switch/app.js
@@ -0,0 +1,18 @@
+let nome = "CArlos  "
+
+switch (nome.toLowerCase().trim()) {
+    case "dionisio":
+        console.log("Achou Dionisio")
+        break
+    case "maria":
+        console.log("Achou Maria")
+        break
+    case "carlos":
+        console.log("Achou Carlos")
+        break
+    default:
+        console.log("Mês inválido")
+    
+
+}
+
